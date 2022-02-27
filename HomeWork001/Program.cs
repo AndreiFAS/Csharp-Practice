6563f64a636c71af39545d6ce3271cab42f7606e
ApkoH <khuzdul@bk.ru>
2022-02-27 17:44:34 +0300

    add HW001

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int max = 0;
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) max = a;
+if (b > a) max = b;
+
+Console.Write(max);
+Console.WriteLine(" наибольшее!");
