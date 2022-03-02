f485d26e817d151507236ebf7ae4f25e1269d811
ApkoH <khuzdul@bk.ru>
2022-03-02 23:02:22 +0300

    add homework006

---
+++ HomeWork006/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int first = 0;
+int second = 0;
+Console.WriteLine("Введите первое число: ");
+first = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine("Введите второе число: ");
+second = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if (first % second == 0)
+{
+    Console.WriteLine("Кратно!");
+}
+else
+{
+    Console.WriteLine("Остаток: " + (second - first));
+}
