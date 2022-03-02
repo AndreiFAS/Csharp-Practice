8703cc2cdccc86ef8813cc1d1092d2d17f3f9e48
ApkoH <khuzdul@bk.ru>
2022-03-02 23:15:05 +0300

    add homework007

---
+++ HomeWork007/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Дано число. Проверить кратно ли оно 7 и 23.
+
+// int number = new Random().Next(10,1000);
+// Console.WriteLine(number);
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 & number % 23 == 0)
+{
+    Console.WriteLine("Кратно!");
+}
+else
+{
+    Console.WriteLine("Некратно!");
+}
