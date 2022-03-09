0c25c741434127bc7830ca90f84d59d23550e956
ApkoH <khuzdul@bk.ru>
2022-03-09 22:01:08 +0300

    add HW010

---
+++ HomeWork010/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int i = 1;
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N > 0)
+{
+    while (i <= N)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+        i++;
+    }
+}
+else
+{
+    while (N <= i)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+        i--;
+    }
+}
