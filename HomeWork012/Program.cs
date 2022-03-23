1c6a5d00ce492c04d690ab51eeb3d4038686941c
ApkoH <khuzdul@bk.ru>
2022-03-23 22:41:35 +0300

    update HW012

--- HomeWork012/Program.cs
+++ HomeWork012/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 Console.WriteLine("Insert number ");
-int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Math.Abs(M);
 string len = Convert.ToString(N);
-int M = N;
 int result = 0;
 int count = 0;
 for (int i = 1; i <= len.Length; i++)
