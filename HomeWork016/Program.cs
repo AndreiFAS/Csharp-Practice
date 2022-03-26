e308e36583872cfdb994f37d347e3051d118c7b8
ApkoH <khuzdul@bk.ru>
2022-03-26 16:04:16 +0300

    add hw016

---
+++ HomeWork016/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 38. Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива
+
+int[] mass = new int [10];
+int max = 0;
+int min = 0;
+
+void FillArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        col[i] = new Random().Next(-99, 100);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write($"{col[i]} ");
+    }
+}
+
+int MaxMin(int[] col)
+{
+for (int i = 0; i < col.Length; i++)
+    {
+       if(col[i] > max) max = col[i];
+       else if(col[i] < min) min = col[i];
+    }
+return max; 
+return min;   
+}
+
+FillArray(mass);
+PrintArray(mass);
+Console.WriteLine();
+max = mass[0];
+min = mass[0];
+MaxMin(mass);
+Console.WriteLine($"Разницу между максимальным {max} и минимальным {min} элементами массива равна {max - min}");
