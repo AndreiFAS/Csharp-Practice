653476985902cf15a0d78d8a30f87cf3bb35bc75
ApkoH <khuzdul@bk.ru>
2022-04-09 17:39:44 +0300

    add HW022

---
+++ HomeWork022/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по 
+// убыванию элементы каждой строки двумерного массива(в пределах всего массива).
+
+int[,] mass = new int[5, 8];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int maxPosition = j;
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, maxPosition]) maxPosition = k;
+            }
+            int temporary = array[i, j];
+            array[i, j] = array[i, maxPosition];
+            array[i, maxPosition] = temporary;
+        }
+    }
+}
+
+FillArray(mass);
+PrintArray(mass);
+Console.WriteLine();
+Console.WriteLine("Отсортированный построчно массив: ");
+SortArrayLines(mass);
+PrintArray(mass);
